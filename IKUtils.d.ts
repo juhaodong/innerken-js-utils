d0041dbc22cb415ffb14ae3662d5a3a1ab449f2f
qi1n <44217604+qi1n@users.noreply.github.com>
2020-11-19 11:47:49 +0100

    add function signature
    add function signature

--- IKUtils.d.ts
+++ IKUtils.d.ts
@@ -1,24 +1,24 @@
 export interface IKUtils{
     ValidateRules,
-    compose,
+    compose():string, //not sure return value
     trick,
-    delCookie,
-    setCookie,
-    getCookie,
-    deepCopy,
-    hideLoading,
-    showLoading,
-    showError,
-    toast,
-    showConfirm,
-    checkKeyExist,
-    safeCallFunction,
-    extend,
-    toggleElement,
-    removeElement,
-    play,
-    getQueryString,
-    wait
+    delCookie(name),
+    setCookie(cName, value:string, expireDays:number),
+    getCookie(name):string, //not sure
+    deepCopy(target:object):object,
+    hideLoading(success:number),
+    showLoading(canCancel:boolean),
+    showError(content:string, title?:string),
+    toast(title:string, type:string),
+    showConfirm(content:string, title?:string ),
+    checkKeyExist(item, key:string):boolean,
+    safeCallFunction(on:object, func, args?:string):function, //not sure
+    extend(target:object, args):object, //not sure
+    toggleElement(el:object, arr:any[]),
+    removeElement(el:object, arr:any[]),
+    play(url),
+    getQueryString(name:string):function, //not sure
+    wait(time):Promise //not sure
 }
 
 export declare const IKUtils:IKUtils
