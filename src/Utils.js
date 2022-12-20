6d2a6c0212f944892abf9b530af2f65326cc0392
Haodong Ju <juhaodong@gmail.com>
2022-12-20 15:04:50 +0100

    Update Utils.js

--- src/Utils.js
+++ src/Utils.js
@@ -222,9 +222,11 @@ export function safeCallFunction (on, func, ...args) {
   return safeCallFunction(this, showError, ('Calling a Invalid Function'))
 }
 
+const singleAudio = new Audio('http://i.cloudup.com/E021I9zUG3.m4a')
+
 export function play (url) {
-  const audio = new Audio(url || 'http://i.cloudup.com/E021I9zUG3.m4a')
-  audio.play()
+  singleAudio.src = url || 'http://i.cloudup.com/E021I9zUG3.m4a'
+  singleAudio.play()
 }
 
 /**
